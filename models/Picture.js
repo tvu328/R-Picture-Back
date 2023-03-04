3f52f8785487d17fe9b3dc9a7efcd6e952bcd8e6
Kailen James <sevenflags57@gmail.com>
2023-03-04 13:02:05 -0800

    Created Picture model

---
+++ models/Picture.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
+
+class Picture extends Model { }
+
+Picture.init({
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+		}
+	},
+	description: {
+		type: DataTypes.TEXT,
+		validate: {
+			notEmpty: true,
+		}
+	},
+	S3URL: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		validate: {
+			//TODO: Validate S3URLS with REGEX
+		}
+	}
+}, {
+	sequelize: sequelize
+});
+
+module.exports = Picture;
