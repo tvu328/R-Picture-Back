73c9ed783d7ff7a5c20214ea88b82275566432b9
Kailen James <sevenflags57@gmail.com>
2023-03-07 14:25:08 -0800

    Created POST route

--- controllers/api/like.js
+++ controllers/api/like.js
@@ -1,29 +1,65 @@
 const express = require("express");
 const router = express.Router();
-const { Like } = require(".../models");
+const { Like } = require("../../models");
 
-router.post("/", async (req,res)=>{
-    try {
+router.post("/", async (req, res) => {
+	try {
+		if (!(req.body.pictureId) || !(req.body.delta)) {
+			return res.sendStatus(400);
+		}
 
-	} catch(error) {
+		if (typeof (req.body.pictureId) !== 'number' || typeof (req.body.delta) !== 'number') {
+			return res.sendStatus(422);
+		}
+
+		const userId = 1;
+		{// Check that a user has not already liked/disliked this picture.
+			const like = await Like.findOne({
+				where: {
+					pictureId: req.body.pictureId,
+					userId: userId
+				}
+			});
+
+			if (like) {
+				return res.sendStatus(422);
+			}
+		}
+		{// Create the like. TODO: This is not atomic and will need to be made into a transaction.
+			const like = await Like.create({
+				delta: req.body.delta,
+				pictureId: req.body.pictureId,
+				userId: userId
+			});
+			return res.sendStatus(204);
+		}
+	} catch (error) {
 		console.log(error);
 		return res.sendStatus(500);
 	}
 });
 
-router.put("/:likeId", async (req,res)=>{
-    try {
+router.put("/:likeId", async (req, res) => {
+	try {
+		if (!(req.body.delta)) {
+			return res.sendStatus(400);
+		}
 
-	} catch(error) {
+		if (typeof (req.body.delta) !== 'number') {
+			return res.sendStatus(422);
+		}
+
+		return res.sendStatus(404);
+	} catch (error) {
 		console.log(error);
 		return res.sendStatus(500);
 	}
 });
 
-router.delete("/:likeId", async (req,res)=>{
-    try {
+router.delete("/:likeId", async (req, res) => {
+	try {
 
-	} catch(error) {
+	} catch (error) {
 		console.log(error);
 		return res.sendStatus(500);
 	}
