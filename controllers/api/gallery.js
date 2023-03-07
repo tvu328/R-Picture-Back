ba25a88c5a66505edf28dda6f0e6a4e1ec0b595e
Kailen James <sevenflags57@gmail.com>
2023-03-07 13:33:03 -0800

    Added routes from issues #28 #32 #33 #34

--- controllers/api/gallery.js
+++ controllers/api/gallery.js
@@ -1,11 +1,146 @@
 const express = require("express");
 const router = express.Router();
-//const {  } = require("../models");
+const { Sequelize } = require("sequelize");
+const { Gallery, User, Like, Picture, Comment } = require("../../models");
 
-router.get("/", (req,res)=>{
-    res.send("This is from gallery controller for testing purpose")
-})
+router.get("/:galleryId", async (req, res) => {
+	try {
+		const picturesOnly = req.query["pictures-only"] || false;
+		const pageLength = req.query["page-length"] || 10;
+		const pageNumber = req.query["page-number"] || 0;
 
+		const userId = 1;
+		const galleryId = req.params.galleryId;
 
+		const gallery = await Gallery.findByPk(galleryId, {
+			attributes: [
+				"id",
+				"name",
+				"description",
+			],
+			include: [
+				{
+					through: {
+						attributes: []
+					},
+					model: User,
+					as: "followedGallery",
+					attributes: ["id"],
+				}, {
+					through: {
+						attributes: []
+					},
+					model: Picture,
+					attributes: ["id", "name", "S3URL"],
+					include: [
+						{
+							model: User,
+							attributes: ["displayName"]
+						}, {
+							model: Comment,
+							attributes: ["id"]
+						}, {
+							model: Like,
+							attributes: ["id", "delta"]
+						}
+					]
+				}
+			],
+		});
+
+		const pictures = gallery.pictures.map(picture => ({
+			id: picture.id,
+			name: picture.name,
+			ownerName: picture.user.displayName,
+			commentCount: picture.comments.length,
+			imageURL: picture.S3URL,
+			score: picture.likes.reduce((score, {delta}) => score + delta, 0),
+			like: picture.likes.find(like => like.id === userId),
+		}));
+
+		return res.status(200).json({
+			id: gallery.id,
+			name: gallery.name,
+			description: gallery.description,
+			//js needs an `istruthy` operator.
+			following: !!gallery.followedGallery.find(user => user.id === userId),
+			followerCount: gallery.followedGallery.length,
+			pictures: pictures,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
+});
+
+router.post("/", async (req, res) => {
+	try {
+		if (!(req.body.name) || !(req.body.description)) {
+			return res.sendStatus(400);
+		}
+		if (typeof (req.body.name) !== 'string' || typeof (req.body.description) !== 'string') {
+			return res.sendStatus(422);
+		}
+
+		const gallery = await Gallery.create({
+			userId: 1,
+			name: req.body.name,
+			description: req.body.description
+		});
+
+		return res.status(201).json({ id: gallery.id, name: gallery.name, description: gallery.description });
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
+});
+
+router.put("/:galleryId", async (req, res) => {
+	try {
+		if ((!(req.body.name) && !(req.body.description)) ||
+			(req.body.name && typeof (req.body.name) !== 'string') ||
+			(req.body.description && typeof (req.body.description) !== 'string')) {
+			return res.sendStatus(422);
+		}
+
+		const [rows] = await Gallery.update({
+			name: req.body.name,
+			description: req.body.description
+		}, {
+			where: {
+				id: req.params.galleryId
+			}
+		});
+
+		if (rows === 0) {
+			return res.sendStatus(404);
+		}
+
+		return res.status(200).json({
+			name: req.body.name,
+			description: req.body.description
+		});
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
+});
+
+router.delete("/:galleryId", async (req, res) => {
+	try {
+		const rows = await Gallery.destroy({
+			where: {
+				id: req.params.galleryId
+			}
+		});
+		if (rows === 0) {
+			return res.sendStatus(404);
+		}
+		return res.status(200).json({ rows: rows });
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
+});
 
 module.exports = router;
