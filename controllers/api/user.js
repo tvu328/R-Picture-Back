c99ad76f631cbc5fa4560e6970f5f98a608bd025
HieuNguyen1905 <120067573+HieuNguyen1905@users.noreply.github.com>
2023-03-08 00:26:36 -0800

    update user routes

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const { User } = require("../../models");
+const { User, Picture, Gallery, Comment, Like } = require("../../models");
 //const jwt = require("jsonwebtoken");
 
 //Get 1 user
@@ -49,7 +49,7 @@ router.post("/", async (req, res) => {
 router.put("/:userId", async (req, res) => {
     try {
         const findUser = await User.findByPk(req.params.userId)
-        if(!findUser){
+        if (!findUser) {
             return res.sendStatus(404)
         }
         if (req.body.newPassword && !req.body.oldPassword) {
@@ -89,10 +89,10 @@ router.delete("/:userId", async (req, res) => {
         if (!user) {
             res.sendStatus(404)
         }
-        if(!req.body.password){
+        if (!req.body.password) {
             return res.sendStatus(400)
         }
-        if(await bcrypt.compare(req.body.password,user.password)){
+        if (await bcrypt.compare(req.body.password, user.password)) {
             const deleteUser = await User.destroy(
                 {
                     where: {
@@ -100,9 +100,162 @@ router.delete("/:userId", async (req, res) => {
                     }
                 })
             res.json(deleteUser)
-        }else{
-        res.sendStatus(403)
+        } else {
+            res.sendStatus(403)
         }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+})
+
+//18
+router.get("/:userId/feed", async (req, res) => {
+    try {
+        const userId = 1;
+        const pageLength = parseInt(req.query["page-length"]) || 10;
+        const pageNumber = parseInt(req.query["page-number"]) || 0;
+
+        const user = await User.findByPk(req.params.userId, {
+            limit: pageLength,
+            offset: pageNumber * pageLength,
+            subQuery: false,
+            include: [{
+                model: Gallery,
+                as: "galleryFollowingUser",
+                include: [{
+                    model: Picture,
+                    attributes: ["id", "name", "S3URL"],
+                    include: [
+						{
+							model: User,
+							attributes: ["displayName"]
+						}, {
+							model: Comment,
+							attributes: ["id"]
+						}, {
+							model: Like,
+							attributes: ["id", "delta"]
+						}
+					]
+                }]
+            },{
+                through: {
+                    attributes: []
+                },
+                model: User,
+                as: "userFollowingUser",
+                attributes: ["id","displayName"],
+                include:[{
+                    model: Picture,
+                    attributes: ["id", "name", "S3URL"],
+                    include: [
+                        {
+                            model: Comment,
+                            attributes: ["id"]
+                        }, {
+                            model: Like,
+                            attributes: ["id", "delta"]
+                        }
+                    ]
+                }]
+            }]
+        })
+
+        const galleryPictures = user.galleryFollowingUser.map(picture => ({
+			id: picture.id,
+			name: picture.name,
+			commentCount: picture.comments.length,
+			imageURL: picture.S3URL,
+			score: picture.likes.reduce((score, { delta }) => score + delta, 0),
+			like: picture.likes.find(like => like.id === userId),
+		}));
+
+        const pictures = user.userFollowingUser.map(picture => ({
+			id: picture.id,
+			name: picture.name,
+			commentCount: picture.comments.length,
+			imageURL: picture.S3URL,
+			score: picture.likes.reduce((score, { delta }) => score + delta, 0),
+			like: picture.likes.find(like => like.id === userId),
+		}));
+
+        const feedPicture = galleryPictures.concat(pictures)
+
+        if (!user) {
+            res.sendStatus(404)
+        }
+
+        const responseJson = {
+            pageLength: pageLength,
+            pageNumber: pageNumber,
+            pictures: feedPicture
+        }
+        res.status(200).json(responseJson)
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+})
+
+//20
+router.get("/:userId/profile", async (req, res) => {
+    try {
+        const userId = 1;
+        const picturesOnly = (req.query["pictures-only"] === 'true') || false;
+        const pageLength = parseInt(req.query["page-length"]) || 10;
+        const pageNumber = parseInt(req.query["page-number"]) || 0;
+
+        const user = await User.findByPk(req.params.userId, {
+            limit: pageLength,
+            offset: pageNumber * pageLength,
+            subQuery: false,
+            attributes: picturesOnly? [] : ["bio", "id", "displayName"],
+            include: [
+                ...(picturesOnly ? [] : [{
+                through: {
+                    attributes: []
+                },
+                model: User,
+                as: "userFollowingUser",
+                attributes: ["id"]
+            }]),
+            {
+                model: Picture,
+                attributes: ["id", "name", "S3URL"],
+                include: [
+                    {
+                        model: Comment,
+                        attributes: ["id"]
+                    }, {
+                        model: Like,
+                        attributes: ["id", "delta"]
+                    }
+                ]
+            }
+            ]
+        })
+
+        const pictures = user.pictures.map(picture => ({
+			id: picture.id,
+			name: picture.name,
+			commentCount: picture.comments.length,
+			imageURL: picture.S3URL,
+			score: picture.likes.reduce((score, { delta }) => score + delta, 0),
+			like: picture.likes.find(like => like.id === userId),
+		}));
+
+        if (!user) {
+            res.sendStatus(404)
+        }
+        res.json(picturesOnly?{pictures:pictures}:{
+            id: user.id,
+            displayName: user.displayName,
+            bio: user.bio,
+            following: !!user.userFollowingUser.find(user => user.id === userId),
+            followerCount: user.userFollowingUser.length,
+            pictures: pictures
+        })
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
