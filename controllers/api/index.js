68a46bb09485a09adef9af28a27d5ea2a1e6e0fc
Hieu Nguyen <120067573+HieuNguyen1905@users.noreply.github.com>
2023-03-09 08:52:54 -0800

    Add jwt (#54)
    * set up stater files
    
    * set up stater files
    
    * update stater files
    
    * update
    
    * add user routes
    
    * update user routes
    
    * update user routes
    
    * update
    
    * update user routes
    
    * update user routes
    
    * add jwt
    
    * add jwt

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { User } = require("../../models")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 
 const searchRoutes = require("./search");
@@ -19,6 +22,43 @@ router.use("/comment", commentRoutes)
 
 const likeRoutes = require("./like")
 router.use("/like", likeRoutes)
+
+router.post("/signin", async (req, res) => {
+    try {
+        if (!req.body.email) {
+            res.sendStatus(400)
+        } else if (!req.body.password) {
+            res.sendStatus(400)
+        }
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        if (!user) {
+            res.sendStatus(403)
+        } else if (!bcrypt.compareSync(req.body.password, user.password)) {
+            res.sendStatus(403)
+        } else {
+            const token = jwt.sign({
+                id: user.id,
+                displayName: user.displayName
+            }, process.env.JWT_SECRET, {
+                expiresIn: "6h"
+            })
+            console.log(token)
+            res.status(201).json(
+                {
+                token: token,
+                user: user
+                }
+            )
+        }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+})
 
 
 module.exports = router;
--- controllers/api/like.js
+++ controllers/api/like.js
@@ -11,27 +11,36 @@ router.post("/", async (req, res) => {
 		if (typeof (req.body.pictureId) !== 'number' || typeof (req.body.delta) !== 'number') {
 			return res.sendStatus(422);
 		}
-
-		const userId = 1;
-		{// Check that a user has not already liked/disliked this picture.
-			const like = await Like.findOne({
-				where: {
-					pictureId: req.body.pictureId,
-					userId: userId
-				}
-			});
-
-			if (like) {
-				return res.sendStatus(422);
+		
+		{
+			const token = req.headers?.authorization?.split(" ")[1];
+			if (!token) {
+				res.sendStatus(403)
 			}
-		}
-		{// Create the like. TODO: This is not atomic and will need to be made into a transaction.
-			const like = await Like.create({
-				delta: req.body.delta,
-				pictureId: req.body.pictureId,
-				userId: userId
-			});
-			return res.sendStatus(204);
+			try {
+				const data = jwt.verify(token, process.env.JWT_SECRET)
+				// Check that a user has not already liked/disliked this picture.
+				const liked = await Like.findOne({
+					where: {
+						pictureId: req.body.pictureId,
+						userId: data.id
+					}
+				});
+	
+				if (liked) {
+					return res.sendStatus(422);
+				}
+				// Create the like. TODO: This is not atomic and will need to be made into a transaction.
+				const like = await Like.create({
+					delta: req.body.delta,
+					pictureId: req.body.pictureId,
+					userId: data.id
+				});
+				return res.sendStatus(204);
+			} catch (err) {
+				console.log(err);
+				return res.status(403).json({ msg: "Invalid or missing token" })
+			}
 		}
 	} catch (error) {
 		console.log(error);
@@ -48,20 +57,30 @@ router.put("/:likeId", async (req, res) => {
 		if (typeof (req.body.delta) !== 'number') {
 			return res.sendStatus(422);
 		}
-
-		const [rows] = await Like.update({
-			delta: req.body.delta
-		}, {
-			where: {
-				id: req.params.likeId
+		const token = req.headers?.authorization?.split(" ")[1];
+        if (!token) {
+            res.sendStatus(403)
+        }
+        try {
+            const data = jwt.verify(token, process.env.JWT_SECRET)
+			const [rows] = await Like.update({
+				delta: req.body.delta
+			}, {
+				where: {
+					id: req.params.likeId,
+					userId: data.id
+				}
+			});
+			console.log(rows);
+			if (rows) {
+				return res.sendStatus(204);
 			}
-		});
-		console.log(rows);
-		if (rows) {
-			return res.sendStatus(204);
-		}
-
-		return res.sendStatus(404);
+	
+			return res.sendStatus(404);
+        } catch (err) {
+            console.log(err);
+            return res.status(403).json({ msg: "Invalid or missing token" })
+        }
 	} catch (error) {
 		console.log(error);
 		return res.sendStatus(500);
@@ -70,16 +89,26 @@ router.put("/:likeId", async (req, res) => {
 
 router.delete("/:likeId", async (req, res) => {
 	try {
-		const rows = await Like.destroy({
-			where: {
-				id: req.params.likeId
+		const token = req.headers?.authorization?.split(" ")[1];
+        if (!token) {
+            res.sendStatus(403)
+        }
+        try {
+            const data = jwt.verify(token, process.env.JWT_SECRET)
+			const rows = await Like.destroy({
+				where: {
+					id: req.params.likeId,
+					userId:data.id
+				}
+			});
+			if (rows === 0) {
+				return res.sendStatus(404);
 			}
-		});
-		if (rows === 0) {
-			return res.sendStatus(404);
-		}
-
-		return res.status(200).json({ rows: rows });
+			return res.status(200).json({ rows: rows });
+        } catch (err) {
+            console.log(err);
+            return res.status(403).json({ msg: "Invalid or missing token" })
+        }
 	} catch (error) {
 		console.log(error);
 		return res.sendStatus(500);
